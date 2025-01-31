5d47e05785afb24682382c0b17ed6f25708fff4e
Jean da Rocha Vertuoso <jean_b123@hotmail.com>
2025-01-31 19:52:32 -0300

    Project created

---
+++ src/test/java/br/com/horadoponto/horadoponto/HoradopontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.horadoponto.horadoponto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoradopontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
